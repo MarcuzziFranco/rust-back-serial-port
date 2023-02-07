59acdb17208d74555a29958175a6cee2b47cb7b9
MarcuzziFranco <marcuzzi7franco@gmail.com>
2023-02-07 13:51:55 -0300

    feat: router endpoint base status,port usb and create connection

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
 use salvo::prelude::*;
 
-#[handler]
-async fn rust_server() -> &'static str {
-    "Server http rust"
-}
 #[tokio::main]
 async fn main() {
-    let router = Router::new().get(rust_server);
+    //let router = Router::new().get(rust_server);
+
+    let router = Router::with_path("serial-data")
+        .get(status_server)
+        .push(Router::with_path("create-connection").get(create_connection))
+        .push(Router::with_path("ports").get(port_usb_active));
+
     Server::new(TcpListener::bind("127.0.0.1:7878"))
         .serve(router)
         .await;
+}
+
+#[handler]
+async fn status_server() -> &'static str {
+    "Server rust is life"
+}
+
+#[handler]
+async fn port_usb_active() -> &'static str {
+    "Port usb active"
+}
+
+#[handler]
+async fn create_connection() -> &'static str {
+    "Connection created"
 }
