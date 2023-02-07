ec317cb46a871325309b9becac3065e2818203db
MarcuzziFranco <marcuzzi7franco@gmail.com>
2023-02-07 13:33:39 -0300

    feat: added initial server http

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use salvo::prelude::*;
+
+#[handler]
+async fn rust_server() -> &'static str {
+    "Server http rust"
+}
+#[tokio::main]
+async fn main() {
+    let router = Router::new().get(rust_server);
+    Server::new(TcpListener::bind("127.0.0.1:7878"))
+        .serve(router)
+        .await;
 }
