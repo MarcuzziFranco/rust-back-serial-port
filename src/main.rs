e1022dc6efb25abb6a6265a02ab1d021a7c69a90
MarcuzziFranco <marcuzzi7franco@gmail.com>
2023-05-02 23:12:28 -0300

    feature add endpoint recived port and baudrate

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,18 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(|| HttpResponse::Ok()))
+            .route("/connect/{port}/{baud_rate}", web::get().to(connect))
             .route("/ports", web::get().to(ports_usb))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+}
+
+async fn connect(req: HttpRequest) -> impl Responder {
+    let port = req.match_info().get("port").unwrap_or("default");
+    let baud_rate = req.match_info().get("baud_rate").unwrap_or("9600");
+    return format!("Connected port:{} with baud rate:{}", &port, &baud_rate);
 }
 
 async fn ports_usb(req: HttpRequest) -> impl Responder {
